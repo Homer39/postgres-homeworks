a1eb10499a9fab21b25d46c57eb32a055b7ec152
Vlad <sapozhnikov.061098@mail.ru>
2023-06-08 16:39:34 +0300

    Выполнены все SQL запросы

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,12 +1,12 @@
 -- SQL-команды для создания таблиц
-CREATE TABLE customers
+CREATE TABLE IF NOT EXISTS customers
 (
 	customer_id char(5) PRIMARY KEY,
 	company_name varchar(100),
 	contact_name varchar(100)
 );
 
-CREATE TABLE employees
+CREATE TABLE IF NOT EXISTS employees
 (
 	employee_id int PRIMARY KEY,
 	first_name varchar(20),
@@ -16,11 +16,11 @@ CREATE TABLE employees
 	notes text
 );
 
-CREATE TABLE orders
+CREATE TABLE IF NOT EXISTS orders
 (
 	order_id int PRIMARY KEY,
-	customer_id char(5) REFERENCES customers,
-	employee_id int REFERENCES employees,
+	customer_id char(5) REFERENCES customers ON DELETE CASCADE,
+	employee_id int REFERENCES employees ON DELETE CASCADE,
 	order_date date,
 	ship_city varchar(50)
 )
